90d765c9a82161aa12acf860e0574d81ebf17d84
LeweC <lewe22lewe@googlemail.com>
2021-03-30 19:23:36 +0200

    First commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.getElementById("save").addEventListener("click", Translate());
+console.log("hier");
+function Translate(){
+  var translateTo = document.getElementById("language").value;
+  console.log("translateTo");
+}
